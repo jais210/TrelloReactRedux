5c23263299ae1114f19001556a2f29a40063b66c
jais210 <jackelineisabel.puruaya@gmail.com>
2017-11-07 12:15:46 -0500

    componentes

---
+++ src/Boards.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {connect} from 'redux-zero/react';
+import {} from './actions';
+import { Form, FormGroup, FormControl, Checkbox, Col, ControlLabel, Button, controlId } from 'react-bootstrap';
+
+const Boards = ({boards})=> (
+    <div className = "singIn">
+    <Form horizontal >
+            
+      <FormGroup controlId="formHorizontalEmail">
+       <Col className = "logo" componentClass={ControlLabel} sm={2}>
+        </Col>
+
+        <Col sm={10}>
+        <FormControl  id="formControlsText" label="Text" className = "input" type="text" placeholder="First name" />
+        </Col>
+
+      <Col sm={10}>
+       <FormControl className = "input" type="text" placeholder="Last name" />
+     </Col>
+
+  <Col sm={10}>
+    <FormControl className = "input" type="email" placeholder="Email" />
+  </Col>
+</FormGroup>
+
+<FormGroup controlId="formHorizontalPassword">
+  <Col componentClass={ControlLabel} sm={2}>          
+  </Col>
+  <Col sm={10}>
+    <FormControl className = "input" type="password" placeholder="Password"/>
+  </Col>
+  <Col sm={10}>
+    <FormControl className = "input" type="password" placeholder="Confirm password"/>
+  </Col>
+</FormGroup>
+
+<FormGroup>
+  <Col smOffset={2} sm={10}>
+    <Button className= "btn" type="submit">
+      Sign in
+    </Button>
+  </Col>
+  <Col smOffset={2} sm={10}>
+  <a className = "a" href="/sign_up" data-reactid=".0.0.0.2">Create new account</a>
+  </Col> 
+         
+</FormGroup>
+</Form>
+
+</div>      
+)
+    
+
+
+
+const mapToProps = ({boards}) => ({boards})
+export default connect(mapToProps)(Boards);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Switch, Route } from 'react-router-dom';
-import App from './App';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import Boards from './Boards';
 import { Provider } from 'redux-zero/react';
 import store from './store';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 
 const Index = () => (
     <Provider store={store}>
        <HashRouter>
           <Switch>
-             <Route path="/" component={App} />
+             <Route path="/" component={SignIn} />
+             <Route  exact path = "/signUp" component={SignUp}/>
+             <Route  exact path = "/boards" component={Boards}/>
           </Switch>
        </HashRouter>
     </Provider>
